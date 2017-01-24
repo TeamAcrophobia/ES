aa27df774e3d4c5ebcc42d558cf18da79dab48b4
Peter Joshua Brion <peterjoshua.brion@gmail.com>
2017-01-25 07:30:28 +0800

    Stuff happened

---
+++ Acrophobia Effector Cardboard/Assets/Scripts/LiftButtonInteraction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LiftButtonInteraction : MonoBehaviour {
+    public float gazeTime = 2.0f;
+
+    private float timer;
+
+    private bool gazedAt;
+
+    GameObject target;
+
+    private bool isElevatorUp = false;
+
+    private bool isPressedButton = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        //do nothing herew
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gazedAt)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= gazeTime)
+            {
+                ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+                timer = 0f;
+            }
+        }
+    }
+
+    public void PointClick()
+    {
+        if (isElevatorUp == false)
+        {
+            target = GameObject.Find("Lift");
+            target.gameObject.GetComponent<Animation>().Play("Elevator1");
+            isElevatorUp = true;
+        }
+    }
+
+    public void PointerExit()
+    {
+        isPressedButton = false;
+    }
+
+    public void PointerEnter()
+    {
+        isPressedButton = true;
+    }
+
+}
---
+++ Acrophobia Effector Cardboard/Assets/Scripts/PlatformButtonInteraction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlatformButtonInteraction : MonoBehaviour {
+	public float gazeTime = 2.0f;
+
+	private float timer;
+
+	private bool gazedAt;
+
+	GameObject target;
+
+	private bool isPlatformMoving = false;
+
+	private bool isPressedButton = false;
+
+	// Use this for initialization
+	void Start()
+	{
+		//do nothing herew
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (gazedAt)
+		{
+			timer += Time.deltaTime;
+
+			if (timer >= gazeTime)
+			{
+				ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerClickHandler);
+				timer = 0f;
+			}
+		}
+	}
+
+	public void PointClick()
+	{
+		if (isPlatformMoving == false)
+		{
+			target = GameObject.Find("1stPlatform");
+			target.gameObject.GetComponent<Animation>().Play("MovingForward");
+			isPlatformMoving = true;
+		}
+	}
+
+	public void PointerExit()
+	{
+		isPressedButton = false;
+	}
+
+	public void PointerEnter()
+	{
+		isPressedButton = true;
+	}
+
+}
